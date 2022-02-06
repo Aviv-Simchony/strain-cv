b6683ae45b90ea32828dfcea73e7b13ad827afcd
Aviv-Simchony <simchony@stanford.edu>
2022-02-05 23:01:13 -0800

    tiny changes

--- mock_jhrecv.py
+++ mock_jhrecv.py
@@ -4,35 +4,21 @@ from PIL import Image
 import numpy as np
 import time
 from matplotlib import cm
-
-
-def noisy(image):
-    row,col,ch= image.shape
-    mean = 0
-    var = 10
-    sigma = var**0.5
-    gauss = np.random.normal(mean,sigma,(row,col,ch))
-    gauss = gauss.reshape(row,col,ch)
-    noisy = image + gauss
-    noisy = noisy/np.max(noisy)
-    return noisy
+import cv2 as cv
 
 
 def jh_recv(out_q):
-
     strain_counter = 0
     while True:
-        if strain_counter > 100:
-            im = Image.open("C:\\Users\\Aviv\\Desktop\\aiohttp_mjpeg-master\\yonghun_strained.jpg") 
-        else:
-            im = Image.open("C:\\Users\\Aviv\\Desktop\\aiohttp_mjpeg-master\\yonghun_strained.jpg") 
-        noisy_image_np = np.asarray(im)
-        noisy_image = Image.fromarray(noisy_image_np)
+        im = Image.open("C:\\Users\\Aviv\\Desktop\\aiohttp_mjpeg-master\\yonghun_cropped.jpg") 
+        stretched_image_np = np.asarray(im)
+        stretched_image_np = cv.resize(stretched_image_np,cv.split(stretched_image_np)[1].shape+np.asarray((int(strain_counter),0)))
+        stretched_image = Image.fromarray(stretched_image_np)
         xio2 = io.BytesIO()
-        im.save(xio2,format="jpeg")
+        stretched_image.save(xio2,format="jpeg")
         time.sleep(1.0/25)
         out_q.put(xio2.getbuffer())
-        strain_counter += 1
+        strain_counter += 0.1
         
 
 
